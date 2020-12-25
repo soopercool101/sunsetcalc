423948ba584514e45a961641f050e7843ed53a46
soopercool101 <soopercoolstages@gmail.com>
2020-12-25 00:10:40 -0800

    Use webpack compile

--- SunsetCalcMain.js
+++
@@ -1,6 +0,0 @@
-const SolarCalc = require('solar-calc');
-const flatpickr = require("flatpickr");
-
-function show(btn) {
-  console.log('Button id:',btn.id);
-}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import SolarCalc from 'solar-calc';
+import FlatPickr from 'flatpickr';
+
+function component() {
+    const element = document.createElement('div');
+    FlatPickr(element, {});
+    return element;
+}
+ 
+ document.body.appendChild(component());
