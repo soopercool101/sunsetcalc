cdeedc964b48c1d1f66e33128a4bce94f3770887
soopercool101 <soopercoolstages@gmail.com>
2020-12-25 01:54:25 -0800

    Clean up output

--- src/index.js
+++ src/index.js
@@ -14,8 +14,11 @@ function change() {
     var solar = new SolarCalc(date, 47.610378, -122.200676);
     var sunset = solar.sunset;
     const MS_PER_MINUTE = 60000;
-    document.getElementById("Before1").innerHTML="1 hr before : " + timeFormat(new Date(sunset - 60 * MS_PER_MINUTE));
-    document.getElementById("Before2").innerHTML="30 min before : " + timeFormat(new Date(sunset - 30 * MS_PER_MINUTE));
+    document.getElementById("Before1").innerHTML="1.5 hr before : " + timeFormat(new Date(sunset - 90 * MS_PER_MINUTE));
+    document.getElementById("Before2").innerHTML="1 hr before : " + timeFormat(new Date(sunset - 60 * MS_PER_MINUTE));
+    document.getElementById("Before3").innerHTML="45 min before : " + timeFormat(new Date(sunset - 45 * MS_PER_MINUTE));
+    document.getElementById("Before4").innerHTML="30 min before : " + timeFormat(new Date(sunset - 30 * MS_PER_MINUTE));
+    document.getElementById("Before5").innerHTML="15 min before : " + timeFormat(new Date(sunset - 15 * MS_PER_MINUTE));
     document.getElementById("Sunset").innerHTML="Sunset : " + timeFormat(sunset);
 }
 
@@ -27,6 +30,10 @@ function timeFormat(date) {
         end = " PM";
         hour = hour - 12;
     }
-    str = hour + ":" + date.getMinutes() + end;
+    str = hour + ":" + minFix(date.getMinutes()) + end;
     return str;
+}
+
+function minFix(min) {
+  return min < 10 ? "0" + min.toString().trim() : min;
 }
