4ed5c53afc9770b9094ecf90269763f3d8958573
soopercool101 <soopercoolstages@gmail.com>
2022-02-24 13:41:07 -0800

    Swap month ranges

--- src/index.js
+++ src/index.js
@@ -16,13 +16,13 @@ function change() {
     const MS_PER_MINUTE = 60000;
     if(date.getMonth() >= 4 && date.getMonth() <= 8)
     {
-        document.getElementById("Before1").innerHTML="2 hr 15 min before : " + timeFormat(new Date(sunset - 135 * MS_PER_MINUTE));
-        document.getElementById("Before2").innerHTML="1 hr 45 before : " + timeFormat(new Date(sunset - 105 * MS_PER_MINUTE));
+        document.getElementById("Before1").innerHTML="2 hr before : " + timeFormat(new Date(sunset - 120 * MS_PER_MINUTE));
+        document.getElementById("Before2").innerHTML="1 hr 30 before : " + timeFormat(new Date(sunset - 90 * MS_PER_MINUTE));
     }
     else
     {
-        document.getElementById("Before1").innerHTML="2 hr before : " + timeFormat(new Date(sunset - 120 * MS_PER_MINUTE));
-        document.getElementById("Before2").innerHTML="1 hr 30 before : " + timeFormat(new Date(sunset - 90 * MS_PER_MINUTE));
+        document.getElementById("Before1").innerHTML="2 hr 15 min before : " + timeFormat(new Date(sunset - 135 * MS_PER_MINUTE));
+        document.getElementById("Before2").innerHTML="1 hr 45 before : " + timeFormat(new Date(sunset - 105 * MS_PER_MINUTE));
     }
     document.getElementById("Sunset").innerHTML="Sunset : " + timeFormat(sunset);
 }
