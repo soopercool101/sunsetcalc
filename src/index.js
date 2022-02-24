b67af8ef883942976c1bfb3bf4c5f06fd1ec1944
soopercool101 <soopercoolstages@gmail.com>
2022-02-24 13:05:04 -0800

    Show different times based on month

--- src/index.js
+++ src/index.js
@@ -14,11 +14,16 @@ function change() {
     var solar = new SolarCalc(date, 47.610378, -122.200676);
     var sunset = solar.sunset;
     const MS_PER_MINUTE = 60000;
-    document.getElementById("Before1").innerHTML="1.5 hr before : " + timeFormat(new Date(sunset - 90 * MS_PER_MINUTE));
-    document.getElementById("Before2").innerHTML="1 hr before : " + timeFormat(new Date(sunset - 60 * MS_PER_MINUTE));
-    document.getElementById("Before3").innerHTML="45 min before : " + timeFormat(new Date(sunset - 45 * MS_PER_MINUTE));
-    document.getElementById("Before4").innerHTML="30 min before : " + timeFormat(new Date(sunset - 30 * MS_PER_MINUTE));
-    document.getElementById("Before5").innerHTML="15 min before : " + timeFormat(new Date(sunset - 15 * MS_PER_MINUTE));
+    if(date.getMonth() >= 4 && date.getMonth() <= 8)
+    {
+        document.getElementById("Before1").innerHTML="2 hr 15 min before : " + timeFormat(new Date(sunset - 135 * MS_PER_MINUTE));
+        document.getElementById("Before2").innerHTML="1 hr 45 before : " + timeFormat(new Date(sunset - 105 * MS_PER_MINUTE));
+    }
+    else
+    {
+        document.getElementById("Before1").innerHTML="2 hr before : " + timeFormat(new Date(sunset - 120 * MS_PER_MINUTE));
+        document.getElementById("Before2").innerHTML="1 hr 30 before : " + timeFormat(new Date(sunset - 90 * MS_PER_MINUTE));
+    }
     document.getElementById("Sunset").innerHTML="Sunset : " + timeFormat(sunset);
 }
 
